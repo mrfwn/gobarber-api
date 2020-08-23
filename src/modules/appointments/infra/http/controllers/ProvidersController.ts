f4b5a0fc48d706943871e562959e6f68fa719ac7
Mário Wessen <mariowessen@gmail.com>
2020-08-23 16:44:08 -0300

    feature: create routers for access availability month and hour with the appointment

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
-export default class ProviddersController {
+export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
 
