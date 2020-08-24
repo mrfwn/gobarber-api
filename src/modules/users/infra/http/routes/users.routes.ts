7c3fd2c17026172c8120d03a1161e39a23138eae
Mário Wessen <mariowessen@gmail.com>
2020-08-24 11:46:08 -0300

    feature: integration with s3 and modify structure for save file in aws s3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 import ensureAutheticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 usersRouter.post(
